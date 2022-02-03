2ab67e6f115d464842184713ec9ccb2944d58c35
Kevin Samoei <samoeikev@gmail.com>
2022-02-03 22:15:15 +0300

    test logs

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/kevinsamoei/aws-lambda-go-example
+
+go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.28.0
+	github.com/golang/glog v1.0.0
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,63 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	log "github.com/sirupsen/logrus"
+	"github.com/golang/glog"
+	"github.com/sirupsen/logrus"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	logger := log.New()
-	log.FieldLogger(logger).Info("field logger")
-	log.WithField("component", "handler").Info("starting handler invocation...")
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+var (
+	logruslogger  = logrus.New()
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
+	InfoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	flag.Set("v", "0")
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+
+}
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
+	fmt.Println("fmt.Println log line")
+	log.Println("log.Println log line")
+	logruslogger.Trace("logruslogger logs in  trace")
+	logruslogger.Debug("logruslogger logs in debug ")
+	logruslogger.Info("logruslogger logs in  info")
+	logruslogger.Warn("logruslogger logs in warn")
+	logruslogger.Error("logruslogger logs in  error")
+
+	InfoLogger.Println("InfoLogger text about info logs")
+	WarningLogger.Println("InfoLogger warn logs")
+	ErrorLogger.Println("InfoLogger error log")
+
+	glog.Info("glog logs in  info")
+	glog.Warning("glog logs in warn")
+	glog.Error("glog logs in  error")
+	glog.Flush()
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode:        200,
+		Headers:           map[string]string{"Content-Type": "text/plain"},
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:              "Hello from Golang",
+		IsBase64Encoded:   false,
 	}, nil
 }
 
 func main() {
-	log.WithField("component", "main").Info("starting function call...")
 	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
 }
